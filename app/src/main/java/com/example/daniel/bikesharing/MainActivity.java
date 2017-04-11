9232622479517e8c65cfb3258243eae85031418a
Daniel <degirolamo.daniel@gmail.com>
2017-04-11 10:44:17 +0200

    AfterDB

--- app/src/main/java/com/example/daniel/bikesharing/MainActivity.java
+++ app/src/main/java/com/example/daniel/bikesharing/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.daniel.bikesharing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.daniel.bikesharing.DB.DatabaseHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Database Helper
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(getApplicationContext());
     }
+
+
 }
